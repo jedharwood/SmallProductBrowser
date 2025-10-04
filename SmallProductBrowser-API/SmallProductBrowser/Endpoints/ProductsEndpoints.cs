ab8268d2cd2f8fad203b911591501504c8c18bf2
JedHarwoodSterlingDcs <jed.harwood@sterling-dcs.com>
2025-10-04 14:25:23 +0100

    Endpoint request logging

--- SmallProductBrowser-API/SmallProductBrowser/Endpoints/ProductsEndpoints.cs
+++ SmallProductBrowser-API/SmallProductBrowser/Endpoints/ProductsEndpoints.cs
@@ -8,23 +8,32 @@ namespace SmallProductBrowser.Endpoints
         {
             var group = routes.MapGroup("/api/products");
 
-            group.MapGet("/", async (IProductsService productsService, string? search, int? page) =>
+            group.MapGet("/", async (ILogger<Program> logger, IProductsService productsService, string? search, int? page) =>
             {
+                logger.LogInformation($"{DateTime.Now}: GET /api/products/ called with query parameters: search=[{search}], page=[{page}]");
                 var products = await productsService.GetProductsAsync(search, page);
 
-                if (products == null)
+                if (products == null) {
+                    logger.LogInformation($"{DateTime.Now}: GET /api/products/ returned Not Found result");
                     return Results.NotFound();
+                }
 
+                logger.LogInformation($"{DateTime.Now}: GET /api/products/ returned: {products.Total} products");
                 return Results.Ok(products);
             });
 
-            group.MapGet("/{id:int}", async (IProductsService productsService, int id) =>
+            group.MapGet("/{id:int}", async (ILogger<Program> logger, IProductsService productsService, int id) =>
             {
+                logger.LogInformation($"{DateTime.Now}: GET /api/products/{id} called");
                 var product = await productsService.GetProductByIdAsync(id);
 
                 if (product == null)
+                {
+                    logger.LogInformation($"{DateTime.Now}: GET /api/products/{id} returned Not Found result");
                     return Results.NotFound();
+                }
 
+                logger.LogInformation($"{DateTime.Now}: GET /api/products/ returned product id:{product.Id}");
                 return Results.Ok(product);
             });
 
--- SmallProductBrowser-API/SmallProductBrowser/Program.cs
+++ SmallProductBrowser-API/SmallProductBrowser/Program.cs
@@ -1,8 +1,20 @@
 using Microsoft.Extensions.Caching.Memory;
 using SmallProductBrowser.Endpoints;
 using SmallProductBrowser.Services;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .WriteTo.Console()
+);
+
 builder.Services.AddMemoryCache();
 builder.Services.AddHttpClient<IProductsService, ProductsService>((sp, httpClient) =>
     new ProductsService(httpClient, sp.GetRequiredService<IMemoryCache>()));
