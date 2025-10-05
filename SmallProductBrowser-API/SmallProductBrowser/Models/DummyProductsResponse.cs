5499f87d5460bff202b47de8de3c4461e38fa92e
JedHarwoodSterlingDcs <jed.harwood@sterling-dcs.com>
2025-10-05 11:32:55 +0100

    refactored product types to remove unused properties, enghancing readability and reducing the amount of data passed from server to client

--- SmallProductBrowser-API/SmallProductBrowser/Models/DummyProductsResponse.cs
+++ SmallProductBrowser-API/SmallProductBrowser/Models/DummyProductsResponse.cs
@@ -2,7 +2,7 @@
 {
     public class DummyProductsResponse
     {
-        public List<DummyProductResponse> Products { get; set; }
+        public List<DummyProductResponse>? Products { get; set; }
         public int Total { get; set; }
         public int Skip { get; set; }
         public int Limit { get; set; }
